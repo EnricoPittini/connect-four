9202e7c7855db5e3e034cff2ad78cd929f382613
EnricoPittini <48480569+EnricoPittini@users.noreply.github.com>
2021-06-09 15:41:16 +0200

    Backend : add http body types

---
+++ backend/src/httpTypes/responses.ts
@@ -0,0 +1,19 @@
+export interface ResponseBody{
+    error: boolean,
+    statusCode: number,
+}
+
+export interface RootResponseBody extends ResponseBody { 
+    apiVersion: string,   
+    endpoints: string[],
+}
+
+export interface LoginResponseBody extends ResponseBody{
+    error: false,
+    token: string,
+}
+
+export interface ErrorResponseBody extends ResponseBody{
+    error: true,
+    errorMessage: string,
+}

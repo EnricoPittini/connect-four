a26783c5bf00728ba71c4f7e1271227f3cda2ab3
EnricoPittini <48480569+EnricoPittini@users.noreply.github.com>
2021-06-11 16:51:51 +0200

    Backend : fix friends routes

--- backend/src/routes/friends.ts
+++ backend/src/routes/friends.ts
@@ -46,24 +46,42 @@ router.get(`/`, auth, (req, res, next) => {
 
 
 router.delete(`/:username`, auth, (req, res, next) => {
-  player.getModel().findOne({ username: req.user?.username }, { friends: 1 }).then((playerDocument) => {
-    if (!playerDocument) {
+  Promise.all([ 
+    player.getModel().findOne({ username: req.user?.username }, { friends: 1 }).exec(),
+    player.getModel().findOne({ username: req.params.username }, { friends: 1 }).exec(),
+  ])
+  .then( ([myPlayerDocument, otherPlayerDocument]) => {
+    if (!myPlayerDocument) {
       console.error('An invalid username asked to delete one of his friend: ' + JSON.stringify(req.user, null, 2));
       const errorBody: ErrorResponseBody = { error: true, statusCode: 500, errorMessage: 'Internal Server error' };
       throw errorBody;
     }
-    const friendRemoved = playerDocument.removeFriend(req.params.username);
-    if (!friendRemoved) {
+    if (!otherPlayerDocument) {
+      console.error('A player asked to delete from the list of his friends an invalid username');
+      const errorBody: ErrorResponseBody = { 
+        error: true, 
+        statusCode: 404, 
+        errorMessage: 'You asked to delete from the list of your friends an invalid username' 
+      };
+      throw errorBody;
+    }
+    let success = myPlayerDocument.removeFriend(req.params.username);
+    success &&= otherPlayerDocument.removeFriend(req.user!.username);
+    if (!success) {
       console.warn('A player is asking to delete a friend that actually isn\'t his friend, user: ' + JSON.stringify(req.user, null, 2));
       const errorBody: ErrorResponseBody = { error: true, statusCode: 404, errorMessage: 'The specified player isn\'t your friend' };
       throw errorBody;
     }
-    return playerDocument.save();
+    return Promise.all([
+      myPlayerDocument.save(),
+      otherPlayerDocument.save(),
+    ]);
   })
   .then(_ => {
     const body: SuccessResponseBody = { error: false, statusCode: 200 };
     return res.status(200).json(body);
-  }).catch((err) => {
+  })
+  .catch((err) => {
     if (err.statusCode) {         // we assume this means it is an ErrorResponseBody
       return next(err);
     }

390c41e133066f9bd2cfa9349c8b4f769e1da1fc
Giacomo Rosin <giacomorosin@gmail.com>
2021-06-19 12:31:41 +0200

    Frontend: add empty sign up component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    SignUpComponent,
   ],
   imports: [
     BrowserModule,

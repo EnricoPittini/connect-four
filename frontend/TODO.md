81280a94f0772fb96e1a8f9380c845a117d62525
Giacomo Rosin <giacomorosin@gmail.com>
2021-06-20 16:36:50 +0200

    Frontend: start implementing friend service

---
+++ frontend/TODO.md
@@ -0,0 +1,24 @@
+# TODO (frontend)
+
+
+## AuthService
+- Add `isAutheticated` method
+
+- Add Auth Guard
+https://medium.com/@ryanchenkie_40935/angular-authentication-using-route-guards-bf7a4ca13ae3
+https://github.com/bartosz-io/jwt-auth-angular/blob/master/src/app/auth/services/auth.service.ts
+
+- Add Role Guard: allow only the roles passed
+
+
+## Nav bar
+- Sarebbe bello inserire un puntino rosso se ci sono nuove notifiche (messaggi / richieste
+  di amicizia). Puntino rosso vicino al link chat / friend request.
+
+
+## Fake data
+- https://randomuser.me/
+
+
+## Env
+- environment.ts
--- frontend/src/app/components/friends-side-bar/friends-side-bar.component.ts
+++ frontend/src/app/components/friends-side-bar/friends-side-bar.component.ts
@@ -1,15 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 
-// TODO da capire se va bene e forse metterla in un file a parte
-interface FriendInfo {
-  username: string,
-  online: boolean,
-  playing: boolean,
-  matchRequestSent: boolean,
-  matchRequestReceived: boolean,
-  // TODO forse unreadMessages: boolean,    // non serve in sidebar, ma in chat
-}
+
 
 /**
  * A side bar that shows information about the player's friends.
@@ -21,10 +13,7 @@ interface FriendInfo {
 })
 export class FriendsSideBarComponent implements OnInit {
 
-  /**
-   * List of FriendInfo.
-   */
-  friends: FriendInfo[] = [];
+
 
   // TODO probabile FriendService
   constructor() { }

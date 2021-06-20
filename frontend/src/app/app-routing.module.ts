78cb882aa6677268354861881c0ea16f7649970a
Giacomo Rosin <giacomorosin@gmail.com>
2021-06-20 11:30:15 +0200

    Frontend: improve home component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/guards/auth.guard';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
+
 
 const routes: Routes = [
   // { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignUpComponent },
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
   // { path: 'detail/:id', component: DetailComponent },
+
+  // TODO eventualmente gestire path errati con 404 not found
+  // {path: '404', component: NotFoundComponent},
+  // {path: '**', redirectTo: '/404'}
 ];
 
 @NgModule({

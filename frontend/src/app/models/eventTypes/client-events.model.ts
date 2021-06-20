417fa85c9e85c3fb6ca41fa94cf9a0f242db13a8
Giacomo Rosin <giacomorosin@gmail.com>
2021-06-20 22:49:10 +0200

    Frontend: implement friend service and add eventTypes

---
+++ frontend/src/app/models/eventTypes/client-events.model.ts
@@ -0,0 +1,47 @@
+export interface FromClientFriendMessage {
+  to : string,
+  text: string
+}
+
+export interface FromClientChatMessage {
+  matchId: string,
+  text: string
+}
+
+/**
+ * Events emitted from the clients.
+ */
+export default interface ClientEvents {
+
+  'online': (jwtToken: string) => void;
+
+  /**
+   * Send a message to the specified username
+   */
+  'friendChat': (message: FromClientFriendMessage) => void; 
+
+  /**
+   * Notify the Server the availability to play a match with the specified friend
+   */
+  'friendMatchRequest': (toUsername : string)=> void;
+
+  /**
+   * Notify the Server the unavailability to play a match with the specified friend
+   */
+  'deleteFriendMatchRequest': (toUsername: string) => void;
+
+  /**
+   * Send a message to the specified match
+   */
+  'matchChat': (text: FromClientChatMessage) => void;
+
+  /**
+   * Notify the Server the availability to play a random match
+   */
+  'randomMatchRequest': () => void;
+
+  /**
+   * Cancel the availability to play a random match
+   */
+  'cancelRandomMatchRequest': ()=> void;
+}

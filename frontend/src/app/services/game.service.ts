02a51ef2a982064487053ea2b4d79f26fa2bab2a
Giacomo Rosin <giacomorosin@gmail.com>
2021-06-21 23:43:45 +0200

    Frontend: improve game service

--- frontend/src/app/services/game.service.ts
+++ frontend/src/app/services/game.service.ts
@@ -9,7 +9,9 @@ import getSocket from 'src/app/utils/initialize-socket-io';
 import { AuthService } from '../auth/services/auth.service';
 import { GetMatchResponseBody, SuccessResponseBody } from '../models/httpTypes/responses.model';
 import { AddMoveRequestBody } from '../models/httpTypes/requests.model';
-import { Match } from '../models/match.model';
+import { Match, MatchStatus } from '../models/match.model';
+import { PlayerService } from './player.service';
+import { Router } from '@angular/router';
 
 
 /**
@@ -34,20 +36,21 @@ export class GameService {
   /**
    * Http headers.
    */
-   private createHttpOptions(params: any = {}) {
-     return {
-       headers: new HttpHeaders({
-         'Authorization': `Bearer ${this.auth.getToken()}`,
-         'Content-Type': 'application/json',
-       }),
-       params: new HttpParams({ fromObject: params }),
-     };
-   };
+  private createHttpOptions(params: any = {}) {
+    return {
+      headers: new HttpHeaders({
+        'Authorization': `Bearer ${this.auth.getToken()}`,
+        'Cache-Control': 'no-cache',
+        'Content-Type': 'application/json',
+      }),
+      params: new HttpParams({ fromObject: params }),
+    };
+  };
 
   /**
    * The socket to interact with the backend.
    */
-  socket: Socket<ServerEvents, ClientEvents>;
+  private socket: Socket<ServerEvents, ClientEvents>;
 
   matchId: string | null;
   match: Match | null;
@@ -55,7 +58,9 @@ export class GameService {
 
   constructor(
     private http: HttpClient,
-    private auth: AuthService
+    private router: Router,
+    private auth: AuthService,
+    private playerService: PlayerService
   ) {
     console.info('Friend service instantiated');
 
@@ -65,6 +70,7 @@ export class GameService {
     // TODO chiedere a backend se sono in game
     this.matchId = null;
     this.match = null;
+    this.initializeMatch();
 
     this.listenForMatchUpdate();
   }
@@ -74,9 +80,17 @@ export class GameService {
   }
 
   makeMove(column: number): void {
-    if (!this.matchId) {
+    // Check that there is a match in progress
+    if (this.match?.status !== MatchStatus.IN_PROGRESS) {
       return;
     }
+
+    // Check that the column is valid
+    if (column < 0 || column >= this.match.board.length) {
+      return;
+    }
+
+    // TODO valutare se fare anche il controllo sul turno
 
     const body: AddMoveRequestBody = {
       column: column,
@@ -110,22 +124,47 @@ export class GameService {
         response => {
           console.info('Match updated succesfully'),
           this.match = response.match;
+          console.log(this.match);
         },
         error => console.error('An error occurred while updating the match')
       );
 
   }
 
+  getPlayer1Username(): string | null {
+    return this.match?.player1 || null;
+  }
+
+  getPlayer2Username(): string | null {
+    return this.match?.player2 || null;
+  }
+
+  isGameEnded(): boolean {
+    return this.match?.status !== MatchStatus.IN_PROGRESS;
+  }
+
+  private initializeMatch(): void {
+    // this.playerService.getPlayer(this.auth.getUsername())
+    // TODO come inizializzo i campi match e matchId ???
+    // TODO forse per semplicità aspetto il primo evento 'match'
+  }
+
   private listenForMatchUpdate(): void {
+    console.info('From now on I\'m listening for match updates')
     // attende mossa
     this.socket.on('match', (matchId) => {
+      // Navigate to the game route
+      this.router.navigate(['/game']);
+      this.matchId = matchId;
       this.updateGame();
     });
 
-    // TODO mi serve il match id
-    // this.socket.on('newMatch', (matchId) => {
-    //   this.matchId = matchId;
-    // });
+    this.socket.on('newMatch', (matchId) => {
+      // Navigate to the game route
+      this.router.navigate(['/game']);
+      this.matchId = matchId;
+      this.updateGame();
+    });
   }
 
   // TODO come gestire invece gli osservatori ???
